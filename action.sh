deb00243be5a50f2480859d9a56ca4d634062c8d
Yan <17621212167@163.com>
2022-03-19 10:42:22 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 rm 1
 rm 2
-time=$(date "+%m%d")
+time=$(date -v-1d "+%m%d")
 echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}.txt")" >> 1
 echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}clash.yml")" >> 2
