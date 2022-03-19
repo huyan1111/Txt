45f3097f71b20404c26ff44e20e9df04567ccfc6
Yan <17621212167@163.com>
2022-03-19 11:59:10 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 rm 1
 rm 2
 time=$(date -d "-1 days" "+%m%d")
-echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}.txt")" >> 1
-echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}clash.yml")" >> 2
+printf "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}.txt")" >> 1
+printf "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}clash.yml")" >> 2
