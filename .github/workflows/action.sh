6a52007828f2d8fe15986371c66ef4ee3ace0f6e
Yan <17621212167@163.com>
2022-03-19 09:46:51 +0800

    Delete action.sh

--- .github/workflows/action.sh
+++
@@ -1,3 +0,0 @@
-time=$(date "+%m%d")
-echo $(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/$time.txt") >> 1
-echo $(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/$timeclash.yml") >> 2
