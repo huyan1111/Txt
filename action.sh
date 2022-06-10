91ffafb507f138fe70cde444678c4525ac45b1d4
Yan <17621212167@163.com>
2022-06-10 19:06:23 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,8 +1,8 @@
 rm 1
 rm 2
 time=$(date -d "-1 days" "+%m%d")
-printf "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}.txt")" >> 1
-printf "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}clash.yml")" >> 2
+echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}.txt")" > 1
+echo "$(curl "https://cdn.jsdelivr.net/gh/pojiezhiyuanjun/freev2@master/${time}clash.yml")" > 2
 
 git config --global user.name 'yan'
 git config --global user.email '17621212167@163.com'
